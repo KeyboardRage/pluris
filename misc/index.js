2f509447b1cfe9b0205572d8a817f9b8409d936f
KeyboardRage <40437596+KeyboardRage@users.noreply.github.com>
2020-11-16 13:18:37 +0100

    Removed console logging
    Not need, only final count is.

--- misc/index.js
+++ misc/index.js
@@ -8,7 +8,6 @@ module.exports = {
   loadPrototype: (Eris, Class, fn, getter = false) => {
     if (Eris[Class].prototype[fn.name] !== undefined) return console.warn(`${fn.name} prototype already exists in ${Class}! ${this.warningMessage('prototype')}`);
     Object.defineProperty(Eris[Class].prototype, fn.name, getter ? { get: fn } : { value: fn });
-    console.log(`Loaded ${Class}#${fn.name}`);
   },
 
   /**
@@ -18,7 +17,6 @@ module.exports = {
   loadImport: (Eris, Class) => {
     if (Eris[Class.name] !== undefined) return console.warn(`Eris.${Class.name} is already defined! ${this.warningMessage('import')}`);
     Object.defineProperty(Eris, Class.name, { value: Class });
-    console.log(`Loaded import ${Class.name}`);
   },
 
   warningMessage: (type) => `The ${type} has not been loaded. Please uninstall/disable any other modules/plugins which creates this override.`,
